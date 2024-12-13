93aa6616929c0dfed54cc43c791ed52c6925821b
Fabio11112 <2123422@student.uma.pt>
2024-12-13 17:14:13 +0000

    a
    aa

--- routes/imageRoute.php
+++ routes/imageRoute.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\imageController;
 
+use App\Models\Utilizador;
+
 $link = 'http://backoffice_er.test/';
 
 
-Route::post('/uploadImage', [imageController::class, 'uploadImages'])->name('image.store');
+Route::post('/uploadImage', [imageController::class, 'uploadImages'])->name('image.upload');
